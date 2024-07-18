287c58570dcbfb8c0dcd54f5f1406b904760f224
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-07-18 08:22:35 -0500

    Adding xml files

--- src/test/java/com/epam/training/sofia_millan/framework/task1/test/GooglePricingCalculatorTest.java
+++ src/test/java/com/epam/training/sofia_millan/framework/task1/test/GooglePricingCalculatorTest.java
@@ -8,6 +8,7 @@ import com.epam.training.sofia_millan.framework.task1.pages.HomePage;
 import com.epam.training.sofia_millan.framework.task1.pages.SearchResultsPage;
 import com.epam.training.sofia_millan.framework.task1.pages.SummaryPage;
 import com.epam.training.sofia_millan.framework.task1.service.InstanceCreator;
+import com.epam.training.sofia_millan.framework.task1.utils.BrowserUtils;
 import com.epam.training.sofia_millan.framework.task1.utils.InstanceConstants;
 import com.epam.training.sofia_millan.framework.task1.utils.TestListener;
 import org.openqa.selenium.WebDriver;
@@ -23,31 +24,42 @@ import static org.testng.Assert.assertTrue;
  * @author Sofía Millán
  */
 @Listeners({TestListener.class})
-public class GoogleTest {
-    static WebDriver driver;
+public class GooglePricingCalculatorTest {
+     WebDriver driver;
 
     /**
      * Initializes the driver before any test methods are executed.
      */
-    @BeforeClass
-    static void setUp(){
+    @BeforeClass(alwaysRun = true)
+    public void setUp(){
         driver = DriverSingleton.getDriver();
     }
 
 
-    /**
-     * This test method:
-     * 1. Opens the Google Cloud home page.
-     * 2. Searches for the Google Cloud Pricing Calculator.
-     * 3. Completes the required form fields to get an estimate.
-     * 4. Obtains and stores the estimate from the calculator.
-     * 5. Shares the estimate to generate a summary.
-     * 6. Obtains and stores the estimate from the summary page.
-     * 7. Compares the calculator's estimate with the summary's estimate to ensure they match,
-     * as well as the values provided in the form.
-     */
+    @Test(groups = {"smoke"})
+    void escenario1(){
+        Instance instance = InstanceCreator.getInstance();
+        SummaryPage summaryPage = flow(instance);
+
+        assertEquals(summaryPage.getValueOf(InstanceConstants.INSTANCE_NUMBER), instance.getNumber());
+        assertEquals(summaryPage.getValueOf(InstanceConstants.GPU_MODEL), instance.getGpuModel());
+        assertEquals(summaryPage.getValueOf(InstanceConstants.REGION), instance.getRegion());
+        assertEquals(summaryPage.getValueOf(InstanceConstants.PROVISIONING_MODEL), instance.getProvisioningModel());
+        assertEquals(summaryPage.getValueOf(InstanceConstants.GPU_NUMBER), instance.getNumberGpus());
+    }
+
     @Test
-    void calculateEstimate(){
+    void escenario2(){
+        Instance instance = InstanceCreator.getInstance();
+        SummaryPage summaryPage = flow(instance);
+
+        assertEquals(summaryPage.getValueOf(InstanceConstants.LOCAL_SSD), instance.getLocalSSD());
+        assertEquals(summaryPage.getValueOf(InstanceConstants.COMMITTED_USE), instance.getCommittedUse());
+        assertEquals(summaryPage.getValueOf(InstanceConstants.OPERATING_SYSTEM), instance.getOperatingSystem());
+        assertTrue(summaryPage.getValueOf(InstanceConstants.MACHINE_TYPE).contains(instance.getMachineType()));
+    }
+
+    private SummaryPage flow(Instance instance){
         HomePage homePage = new HomePage(driver);
         homePage.openPage();
         homePage.performSearch();
@@ -55,35 +67,18 @@ public class GoogleTest {
         SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
         searchResultsPage.followLinkToCalculator();
 
-        Instance instance = InstanceCreator.getInstance();
-
         CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.fillForm(instance);
-        double calculatorEstimate = calculatorPage.getEstimate();
         calculatorPage.shareEstimate();
-
-        SummaryPage summaryPage = new SummaryPage(driver);
-        double summaryEstimate = summaryPage.getEstimate();
-
-        //assertEquals(calculatorEstimate, summaryEstimate*2); //If this line is uncommented the test will fail
-        assertEquals(calculatorEstimate, summaryEstimate);
-        assertEquals(summaryPage.getValueOf(InstanceConstants.INSTANCE_NUMBER), instance.getNumber());
-        assertEquals(summaryPage.getValueOf(InstanceConstants.GPU_MODEL), instance.getGpuModel());
-        assertEquals(summaryPage.getValueOf(InstanceConstants.REGION), instance.getRegion());
-        assertEquals(summaryPage.getValueOf(InstanceConstants.PROVISIONING_MODEL), instance.getProvisioningModel());
-        assertEquals(summaryPage.getValueOf(InstanceConstants.GPU_NUMBER), instance.getNumberGpus());
-        assertEquals(summaryPage.getValueOf(InstanceConstants.LOCAL_SSD), instance.getLocalSSD());
-        assertEquals(summaryPage.getValueOf(InstanceConstants.COMMITTED_USE), instance.getCommittedUse());
-        assertEquals(summaryPage.getValueOf(InstanceConstants.OPERATING_SYSTEM), instance.getOperatingSystem());
-        assertTrue(summaryPage.getValueOf(InstanceConstants.MACHINE_TYPE).contains(instance.getMachineType()));
-
+        BrowserUtils.changeTab(driver,1);
+        return new SummaryPage(driver);
     }
 
     /**
      * Closes all driver windows after all test methods have been executed.
      */
-    @AfterClass
-    static void tearDown(){
+    @AfterClass(alwaysRun = true)
+    public void tearDown(){
         DriverSingleton.closeDriver();
     }
 }
